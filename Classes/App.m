5f3dd4ff94a36ea699615b5fc416d5a97a39b3fb
Luca Simone Gatta de Marinis <loop23@gmail.com>
2019-05-10 17:58:30 +0200

    Initial release

---
+++ Classes/App.m
@@ -0,0 +1,66 @@
+#import "App.h"
+
+#import "NSURL+L0URLParsing.h"
+
+@implementation App
+
+-(void)awakeFromNib {
+    // NSUserDefaults *d = [NSUserDefaults standardUserDefaults];
+    path = @"/usr/bin/open"; // [d objectForKey:@"path"];
+
+    NSAppleEventManager *appleEventManager = [NSAppleEventManager sharedAppleEventManager];
+    [appleEventManager setEventHandler:self andSelector:@selector(handleGetURLEvent:withReplyEvent:) forEventClass:kInternetEventClass andEventID:kAEGetURL];
+}
+
+-(void)handleGetURLEvent:(NSAppleEventDescriptor *)event withReplyEvent:(NSAppleEventDescriptor *)replyEvent {
+    if (nil == path) return;
+
+    // emacs://open/?url=file://~/.bash_profile&line=11&column=2
+    NSURL *url = [NSURL URLWithString:[[event paramDescriptorForKeyword:keyDirectObject] stringValue]];
+
+    if (url && [[url host] isEqualToString:@"open"]) {
+        NSDictionary *params = [url dictionaryByDecodingQueryString];
+        NSURL *file_url = [NSURL URLWithString:[params objectForKey:@"url"]];
+
+        if (file_url && [[file_url scheme] isEqualToString:@"file"]) {
+            NSString *file = [file_url path];
+            // NSString *line = [params objectForKey:@"line"];
+            // NSString *column = [params objectForKey:@"column"];
+
+            if (file) {
+                NSTask *task = [[NSTask alloc] init];
+                [task setLaunchPath:path];
+                [task setArguments:[NSArray arrayWithObjects:file, nil]];
+                [task launch];
+                [task release];
+            }
+        }
+    }
+
+    if (![prefPanel isVisible]) {
+        [NSApp terminate:self];
+    }
+}
+
+-(IBAction)showPrefPanel:(id)sender {
+    if (path) {
+        [textField setStringValue:path];
+    }
+    else {
+        [textField setStringValue:@""];
+    }
+    [prefPanel makeKeyAndOrderFront:nil];
+}
+
+-(IBAction)applyChange:(id)sender {
+    path = [textField stringValue];
+
+    if (path) {
+        NSUserDefaults *d = [NSUserDefaults standardUserDefaults];
+        [d setObject:path forKey:@"path"];
+    }
+
+    [prefPanel orderOut:nil];
+}
+
+@end
---
+++ README.md
@@ -0,0 +1,68 @@
+TheFactoryOpener for OSX
+=======================
+
+This application enables your emacs to open files via hyperlinks starts with `thefac://` something like:
+
+    thefac://open/?url=file:///etc/passwd&line=10&column=2
+
+This URL format is same as TextMate's `txmt:` scheme described [here](http://manual.macromates.com/en/using_textmate_from_terminal#url_scheme_html)
+
+This app is wholly derivative of https://github.com/typester/emacs-handler which did the same(ish) for emacs: scheme urls
+
+Requirements
+------------
+
+* Mac OS X (10.5 or 10.6)
+
+Download
+--------
+
+<!--
+[EmacsHandler-1.0.zip](http://cloud.github.com/downloads/typester/emacs-handler/EmacsHandler-1.0.zip) (Universal Binary)
+
+Installation
+------------
+
+Download [latest release](http://cloud.github.com/downloads/typester/emacs-handler/EmacsHandler-1.0.zip).
+
+Unzip it, and install it in your Applications directory.
+
+Launch it and select `EmacsHandler` -> `Preferences...`, then following window will open.
+
+<a href="http://www.flickr.com/photos/typester/4060516152/" title="EmacsHandler Preferences by typester, on Flickr"><img src="http://farm3.static.flickr.com/2561/4060516152_16d3370a7a_o.png" width="480" height="252" alt="EmacsHandler Preferences" /></a>
+
+Enter your emacsclient path and push apply button.
+
+Test it
+-------
+
+Open terminal and type:
+
+    open 'emacs://open/?url=file:///etc/hosts'
+
+or just click [this link](emacs://open/?url=file:///etc/hosts)
+-->
+
+Uninstalling
+------------
+
+Delete following:
+
+    /Applications/EmacsHandler.app
+    ~/Library/Preferences/org.unknownplace.emacshandler.plist
+
+
+Original Author
+------
+
+Daisuke Murase :@typester (github, twitter, CPAN, etc..)
+
+Author
+-----
+
+Luca Loop de Marinis
+
+License
+-------
+
+BSD.
